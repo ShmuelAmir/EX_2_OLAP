7b8da4df465b45779ba86fe0d295c905f970b81c
Shmuel Amir <shmulik5777@gmail.com>
2023-03-21 11:04:56 +0200

    plot 1 finish

--- script/Week3_power.R
+++ script/Week3_power.R
@@ -7,11 +7,9 @@ library(lubridate)
 # ---------- INPUT DATA ---------- #
 
 A <- read.delim('data/table.tsv')
-
 # arrange the data
 A$DateTime <- as.POSIXct( A$megawatthours, tz = "EST", format = "%H:%M EST %m/%d/%Y" )
 B <- A[ order(A$DateTime), ]
-
 
 # ---------- Q1 ---------- #
 
@@ -19,26 +17,22 @@ B <- A[ order(A$DateTime), ]
 C <- B[B$DateTime >= as.POSIXct("2021-02-07", tz = "EST") 
        & B$DateTime <= as.POSIXct("2021-02-13 23:59:59", tz = "EST") ,]
 
+C[, grepl("^Net.generation", names(C))]
 # sum net generation across the US
-net_generation_cols <- c(
-  "Net.generation",
-  "Net.generation.1",
-  "Net.generation.2",
-  "Net.generation.3",
-  "Net.generation.4",
-  "Net.generation.5",
-  "Net.generation.6",
-  "Net.generation.7",
-  "Net.generation.8",
-  "Net.generation.9",
-  "Net.generation.10"
-)
-C$Total_net_generation <- rowSums(C[,net_generation_cols])
+C$Total_net_generation <- rowSums(select(C, starts_with("Net.generation")))
 
 # prepare the axis
 C <- C %>%
     group_by(date = as.Date(DateTime)) %>%
     summarise(mean_net_generation = mean(Total_net_generation))
+
+plot(C$date, C$mean_net_generation,
+     xlab = "Date (7-14 Feb)",
+     ylab = "Mean Net Generation",
+     main = "Mean daily power generation across US, 7-14 Feb",
+     type = "o")
+
+# linear model line??????
 
 
 # ---------- Q2 ---------- #
@@ -60,6 +54,8 @@ E <- E %>%
     summarise(demand = sum(Demand))
 
 E <- na.omit(E)
+
+#plot(E$period, E$demand)
 
 
 # ---------- OUTPUT PLOTS ---------- #
