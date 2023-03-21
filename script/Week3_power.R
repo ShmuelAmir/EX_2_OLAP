1d09439bfca4d185890bcf1fccd906ac21a8e600
Shmuel Amir <shmulik5777@gmail.com>
2023-03-21 11:07:27 +0200

    plot 1 finished and saved

--- script/Week3_power.R
+++ script/Week3_power.R
@@ -1,77 +1,82 @@
 library(dplyr)
 library(ggplot2)
-library(plotly)
-library(lubridate)
+#library(plotly)
+#library(lubridate)
+
+#pdf('output/Week3_power.pdf')
 
 
 # ---------- INPUT DATA ---------- #
 
 A <- read.delim('data/table.tsv')
-# arrange the data
-A$DateTime <- as.POSIXct( A$megawatthours, tz = "EST", format = "%H:%M EST %m/%d/%Y" )
-B <- A[ order(A$DateTime), ]
 
+# add more readable DateTime columns,
+# individual columns for date and hour
+# filter to the week of 7 - 14 Feb. 2021,
+# and order by DateTime
+B <- A %>%
+  mutate(DateTime = as.POSIXct(megawatthours, tz = "EST", format = "%H:%M EST %m/%d/%Y")) %>%
+  mutate(Date = as.Date(DateTime), Hour = format(DateTime, format = "%H")) %>%
+  filter(Date >= as.Date("2021-02-07"),
+         Date <= as.Date("2021-02-13")) %>%
+  arrange(DateTime)
+  
+  
 # ---------- Q1 ---------- #
 
-# filter the requested week
-C <- B[B$DateTime >= as.POSIXct("2021-02-07", tz = "EST") 
-       & B$DateTime <= as.POSIXct("2021-02-13 23:59:59", tz = "EST") ,]
+# Date vs mean of total net generation
+C <- B %>%
+  mutate(Total.net.generation = 
+         Net.generation +
+         Net.generation.1 +
+         Net.generation.2 +
+         Net.generation.3 +
+         Net.generation.4 +
+         Net.generation.5 +
+         Net.generation.6 +
+         Net.generation.7 +
+         Net.generation.8 +
+         Net.generation.9 +
+         Net.generation.10
+  ) %>%
+  group_by(Date) %>%
+  summarize(Mean_Power = mean(Total.net.generation))
 
-C[, grepl("^Net.generation", names(C))]
-# sum net generation across the US
-C$Total_net_generation <- rowSums(select(C, starts_with("Net.generation")))
-
-# prepare the axis
-C <- C %>%
-    group_by(date = as.Date(DateTime)) %>%
-    summarise(mean_net_generation = mean(Total_net_generation))
-
-plot(C$date, C$mean_net_generation,
-     xlab = "Date (7-14 Feb)",
-     ylab = "Mean Net Generation",
-     main = "Mean daily power generation across US, 7-14 Feb",
-     type = "o")
-
-# linear model line??????
+ggplot(C, aes(x = Date, y = Mean_Power), xla) +
+  xlab("Date (7-14 Feb)") +
+  ylab("Mean Net Generation") +
+  geom_line() + 
+  ggtitle("Mean daily power generation") +
+  geom_point() + 
+  geom_smooth(method = "lm", formula = y ~ x, se=FALSE)
 
 
 # ---------- Q2 ---------- #
-
-# combine east coast power demand
-east_coast_cols <- c("Demand.9", "Demand.7", "Demand.5", "Demand.4", "Demand.2")
-
-D <- B
-D$east_coast_demand <- rowSums(D[ , east_coast_cols ])
-
-E <- na.omit(data.frame(hour = as.numeric(format(as.POSIXct(D$DateTime), format = "%H"))
-                        , Demand = D$east_coast_demand))
-
-# group by day and night
-E <- E %>%
-  mutate(period = case_when(hour >= 10 & hour <= 18 ~ "day",
-                            hour >= 20 | hour <= 3 ~ "night")) %>%
-    group_by(period) %>%
-    summarise(demand = sum(Demand))
-
-E <- na.omit(E)
-
-#plot(E$period, E$demand)
+# גרף אחד ליום וגרף אחד ללילה לפי דקות. רק לשבוע הנ"ל.
+# להכל צריך את נוסחת הרגרסיה
+# data.frame חדש
+# cube = matrix
 
 
-# ---------- OUTPUT PLOTS ---------- #
+# combine east coast power demand 
+D <- B %>%
+  mutate(east_coast_demand = Demand.9 + Demand.7 + Demand.5 + Demand.4 + Demand.2) %>%
+  group_by(Hour) %>%
+  summarize(minut_power_demand = sum(east_coast_demand))
 
-#pdf('output/Week3_power.pdf')
+D.day <- D %>%
+  filter(as.numeric(Hour) >= 10 & as.numeric(Hour) <= 18)
 
-# draw the first plot
-#ggplot(C, aes(x = date, y = mean_net_generation)) + 
- # geom_line() +
-#  labs(x = "Day", y = "Mean Net generation") +
-  #scale_x_date(date_labels = "%d", date_breaks = "1 day") +
- # ggtitle("Mean daily power generation across US, 7-14 Feb")
+D.night <- D %>%
+  filter(as.numeric(Hour) >= 20 | as.numeric(Hour) <= 3)
 
-# draw the first plot
-ggplot(data=E, aes(x=period, y=demand, fill=period)) +
-  geom_bar(stat = "identity")
-#ggplotly(bar)
+ggplot(D.day, aes(x = Hour, y = minut_power_demand)) +
+  geom_point() +
+  geom_line()
+
+ggplot(D.night, aes(x = Hour, y = minut_power_demand)) +
+  geom_point() +
+  geom_line()
+
 
 #dev.off()
