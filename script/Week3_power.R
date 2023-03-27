d6b5acc3464b6c8c4bcb738f7a9fbd85becfb40c
Shmuel Amir <shmulik5777@gmail.com>
2023-03-27 20:54:42 +0300

    start trying the teacher way

--- script/Week3_power.R
+++ script/Week3_power.R
@@ -1,13 +1,51 @@
 library(dplyr)
 library(ggplot2)
+library(lubridate)
 
-pdf('output/Week3_power.pdf')
+#pdf('output/Week3_power.pdf')
 Sys.setlocale("LC_TIME", "English")
 
 # ---------- INPUT DATA ---------- #
 
-A <- read.delim('data/table.tsv')
+electric_data_frame <- read.delim('data/table.tsv')
 
+slice.electric_data_frame <- electric_data_frame %>%
+  mutate(DateTime = as.POSIXct(megawatthours, tz = "EST", format = "%H:%M EST %m/%d/%Y")) %>%
+  mutate(Date = as.Date(DateTime), Hour = format(DateTime, format = "%H")) %>%
+  select(Date, Hour, DateTime, 
+         starts_with("Net.generation"),
+         Demand.9, Demand.7, Demand.5, Demand.4, Demand.2
+  ) %>%
+  filter(Date >= as.Date("2021-02-07"), Date <= as.Date("2021-02-13")) %>%
+  arrange(DateTime)
+
+net.generation.df <- slice.electric_data_frame %>%
+  select(starts_with("Net.generation"))
+
+electric_fact <- as.matrix(net.generation.df)
+
+net.generation.names.list <- as.list(
+  sapply(names(slice.electric_data_frame)[4:14], function(x) x = NA))
+
+# mean values
+M <- net.generation.names.list
+# standard deviations
+S <- net.generation.names.list
+# linear fit
+LM <- net.generation.names.list
+
+# calculate means and stdev
+for (i in 1:length(names(net.generation.names.list))) {
+  curr.net.generation <- net.generation.df[, i]
+  M[[ i ]] <- mean( curr.net.generation )
+  S[[ i ]] <- sd( curr.net.generation )
+}
+
+# scale and center the consumption series (normalize)
+norm.net.generation.df <- t( (t(net.generation.df) - unlist(M)) / unlist(S) )
+print(norm.net.generation.df)
+
+# לתקן את אזור הזמן  ??????????
 # add columns: DateTime, Date and Hour,
 # select only to be used columns,
 # filter to the week of 7 - 14 Feb. 2021,
@@ -19,7 +57,7 @@ B <- A %>%
          starts_with("Net.generation"),
          Demand.9, Demand.7, Demand.5, Demand.4, Demand.2
         ) %>%
-  filter(Date >= as.Date("2021-02-07"),
+  filter(Date >= as.Date("2021-02-07"), # slice
          Date <= as.Date("2021-02-13")) %>%
   arrange(DateTime)
   
@@ -73,11 +111,14 @@ D <- B %>%
 D.day <- filter(D, Day_or_night == "Day")
 D.night <- filter(D, Day_or_night == "Night")
 
-coef_lm.day <- coef(lm(minut_power_demand ~ hour_as_num, data = D.day[1:8, ])) # remove outliers
+# remove outliers (if D.day[:7,] the correlation increases even more)
+coef_lm.day <- coef(lm(minut_power_demand ~ hour_as_num, data = D.day[1:8, ]))
 intercept.day <- coef_lm.day[1]
 slope.day <- coef_lm.day[2]
 
-coef_lm.night <- coef(lm(minut_power_demand ~ factor(hour_as_num, levels = c(10:23, 0:3)), data = D.night))
+coef_lm.night <- coef(lm(
+  minut_power_demand ~ factor(hour_as_num, levels = c(10:23, 0:3)), 
+  data = D.night))
 intercept.night <- coef_lm.night[1]
 slope.night <- coef_lm.night[2]
 
